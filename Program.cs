0a06174185dcda320200d5e0a576aff8c94cdcce
JanCarlos2772 <jancarlos2715@gmail.com>
2025-03-03 20:51:56 -0400

    Antes del Intento de poner 2 condiciones combinadas en 1

--- Program.cs
+++ Program.cs
@@ -6,5 +6,47 @@ class Program
     {
         Console.WriteLine("Hello, World!");
         Console.WriteLine("Cambio 1");
+
+        Random dado = new Random();
+        int roll1 = dado.Next(1,7);
+        int roll2 = dado.Next(1,7);
+        int roll3 = dado.Next(1,7);
+
+        int total = roll1 + roll2 + roll3;
+
+        Console.WriteLine($"Tirada de Dados: {roll1} + {roll2} + {roll3} = {total}");
+        if (((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) && ((roll1 == roll2) && (roll2 == roll3)))
+        {
+            
+        }
+        if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3))
+        {
+            Console.WriteLine("You rolled doubles! +2 bonus to total!");
+            total += 2;
+        } 
+        if((roll1 == roll2) && (roll2 == roll3))
+        {
+            Console.WriteLine("You rolled triples! +6 bonus to total!");
+            total += 6;
+        }
+        if(total >= 15)
+        {
+            Console.WriteLine("You win!");
+        }
+        if(total < 15)
+        {
+            Console.WriteLine("Sorry, you lose.");
+        }
+        string message = "The quick brown fox jumps over the lazy dog.";
+        bool result = message.Contains("dog");
+        Console.WriteLine(result);
+
+        if (message.Contains("fox"))
+        {
+            Console.WriteLine("What does the fox say?");
+        }
+
+
+
     }
 }
